c219ee4a8330d0bd55d0641253d8a8ceb365c8b3
Usaid Ahmed <usaid33376@gmail.com>
2026-05-08 14:43:45 +0500

    update

--- docker_implementation/airflow_project/scripts/load_to_mysql.py
+++ docker_implementation/airflow_project/scripts/load_to_mysql.py
@@ -4,23 +4,36 @@ import os
 
 print("Starting MySQL load...")
 
+# ---------------------------
+# DATABASE CONFIG
+# ---------------------------
 connection = pymysql.connect(
     host="host.docker.internal",
     user="root",
     password="usaid123",
     database="week3_db"
 )
+
 cursor = connection.cursor()
 
+# ---------------------------
+# PATH SETUP
+# ---------------------------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(BASE_DIR)
 
 processed_path = os.path.join(PROJECT_ROOT, "data", "processed", "cleaned_api_data.csv")
 
+# ---------------------------
+# LOAD DATA
+# ---------------------------
 df = pd.read_csv(processed_path)
 
 print("Data loaded.")
 
+# ---------------------------
+# CREATE TABLE
+# ---------------------------
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS api_data (
     userId INT,
@@ -30,8 +43,13 @@ CREATE TABLE IF NOT EXISTS api_data (
     title_length INT
 )
 """)
+
+# Clear old data
 cursor.execute("TRUNCATE TABLE api_data")
 
+# ---------------------------
+# INSERT DATA
+# ---------------------------
 for _, row in df.iterrows():
     cursor.execute("""
         INSERT INTO api_data (userId, id, title, body, title_length)
--- docker_implementation/airflow_project/scripts/upload_to_s3.py
+++ docker_implementation/airflow_project/scripts/upload_to_s3.py
@@ -2,14 +2,14 @@ import boto3
 
 s3 = boto3.client(
     's3',
-    #aws_access_key_id='your key id',
-    #aws_secret_access_key='your secret access',
-    #region_name='your region'#
+    aws_access_key_id='AKIA3TF4LH5RLW4FPUIT',
+    aws_secret_access_key='i9Fnn4MziiopqMLpDUD2PrIiZ9LSVs5ZFnqNOeGc',
+    region_name='eu-north-1'
 )
 
 file_name = "/opt/airflow/data/processed/cleaned_api_data.csv"
-bucket_name = #"usaid-data-pipeline-bucket"
-object_name = #"cleaned_api_data.csv"
+bucket_name = "usaid-data-pipeline-bucket"
+object_name = "cleaned_api_data.csv"
 
 s3.upload_file(file_name, bucket_name, object_name)
 
