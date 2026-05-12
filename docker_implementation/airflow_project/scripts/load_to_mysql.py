36b1e2e0530996e7e8435442ff0eeb6405ad803e
Usaid Ahmed <usaid33376@gmail.com>
2026-05-12 16:15:36 +0500

    Update load_to_mysql.py

--- docker_implementation/airflow_project/scripts/load_to_mysql.py
+++ docker_implementation/airflow_project/scripts/load_to_mysql.py
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS api_data (
 
 cursor.execute("TRUNCATE TABLE api_data")
 
-
+# ---------------------------
 # INSERT DATA
 # ---------------------------
 for _, row in df.iterrows():
