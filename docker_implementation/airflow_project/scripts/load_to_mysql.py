52f2f4eefd6e18797139918a1803d3fcbcd4f1d0
Usaid Ahmed <usaid33376@gmail.com>
2026-05-12 16:15:19 +0500

    Update load_to_mysql.py

--- docker_implementation/airflow_project/scripts/load_to_mysql.py
+++ docker_implementation/airflow_project/scripts/load_to_mysql.py
@@ -16,24 +16,23 @@ connection = pymysql.connect(
 
 cursor = connection.cursor()
 
-# ---------------------------
-# PATH SETUP
+
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(BASE_DIR)
 
 processed_path = os.path.join(PROJECT_ROOT, "data", "processed", "cleaned_api_data.csv")
 
-# ---------------------------
-# LOAD DATA
-# ---------------------------
+
+
 df = pd.read_csv(processed_path)
 
 print("Data loaded.")
 
-# ---------------------------
-# CREATE TABLE
-# ---------------------------
+
+
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS api_data (
     userId INT,
@@ -44,12 +43,11 @@ CREATE TABLE IF NOT EXISTS api_data (
 )
 """)
 
-# Clear old data
+
 cursor.execute("TRUNCATE TABLE api_data")
 
-# ---------------------------
-# INSERT DATA
-# ---------------------------
+
+
 for _, row in df.iterrows():
     cursor.execute("""
         INSERT INTO api_data (userId, id, title, body, title_length)
